18c62d9796c9d8d2d7a68993c6449c7386f2a3c6
Frankie <geos_tricks@outlook.com>
2024-03-15 05:22:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Write a function that takes in an array and sort the numbers inside from least to greatest
+
+let numArray = [576, 250, 4, 7528, 22637, 57];
+numArray.sort((a, b) => a - b);
+console.log(numArray)
